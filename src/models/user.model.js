e56ffbe8d607fd47f02ebf8fddecc58452b24389
sakshi <sakshikumaree44@gmail.com>
2026-09-11 12:00:52 +0530

    addmodels

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from "mongoose";
+import jwt from  "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema=new Schema(
     
@@ -21,7 +23,7 @@ const userSchema=new Schema(
             
             
         },
-           fullname:{
+           fullName:{
             type:String,
             required:true,
             unique:true,
@@ -60,6 +62,37 @@ const userSchema=new Schema(
      
     
 )
+userSchema.pre("save",async function(next) {
+    if(this.isModified("password"))return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect=async function(){
+   return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAcesstoken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+        
+    },
+    process .env.ACCESS_TOKEN_SECRET,{
+        expiresIn:process.env.ACCESS_TOKEN_SECRET,
+    }
+)
+}
+userSchema.methods.generateRefeshtoken=function(){
+      return jwt.sign({
+        _id:this._id,
+        
+        
+    },
+    process .env.REFRESH_TOKEN_SECRET,{
+        expiresIn:process.env.REFRESH_TOKEN_SECRET,
+    }
+}
 
 
 export const User=mongoose.model("User",userSchema)
