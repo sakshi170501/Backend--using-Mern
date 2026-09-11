961ab210132a61873f7865413e52eecc6aea4ad0
sakshi <sakshikumaree44@gmail.com>
2026-09-11 10:25:37 +0530

    models

---
+++ src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema=new Schema(
+    
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true,
+            
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            
+            
+        },
+           fullname:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+    
+            
+        },
+           avatar:{
+            type:String,//cloudinary url
+            required:true,
+
+          
+        },
+        coverImage:{
+            type:String //cloudinary url
+        },
+        watchHistory:[
+            {
+               type:Schema.Types.ObjectId,
+               REF:"Video" 
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,'Password is required']
+        },
+        refreshToken:{
+            type:String
+        }
+    },
+        {
+            timestamps:true
+        }
+    
+     
+    
+)
+
+
+export const User=mongoose.model("User",userSchema)
